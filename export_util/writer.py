b22d1ebf90a2265870405fbf70fe670e8f7e65f6
velimir <vorlinov@bmat.com>
2020-04-15 18:21:23 +0200

    adding simple pdf support

--- export_util/writer.py
+++ export_util/writer.py
@@ -6,6 +6,12 @@ import openpyxl
 from openpyxl.writer.excel import ExcelWriter
 from openpyxl import Workbook, load_workbook
 
+## PDF
+import itertools
+from reportlab.lib.pagesizes import A4, landscape
+from reportlab.pdfgen import canvas
+##
+
 
 class OutputTemplate:
     """
@@ -193,9 +199,59 @@ class CSVBytesOutputWriter(BytesOutputWriter):
         return self.output.getvalue()
 
 
+class PDFBytesOutputWriter(BytesOutputWriter):
+    """
+    PDF Data Writer.
+    """
+    mime_type = 'text/pdf'
+    extension = 'pdf'
+
+    def grouper(self, iterable, n):
+        args = [iter(iterable)] * n
+        return itertools.zip_longest(*args)
+
+    def __init__(self, offsets):
+        super(PDFBytesOutputWriter, self).__init__()
+
+        self.output = BytesIO()
+        self.writer = canvas.Canvas(self.output, pagesize=landscape(A4))
+
+        self.offsets = offsets
+        self.data = []
+
+    def write(self, *cols):
+        print(cols)
+        self.data.append(cols)
+
+
+    def get_data(self):
+        w, h = landscape(A4)
+        max_rows_per_page = 45
+        # Margin.
+        x_offset = 50
+        y_offset = 50
+        # Space between rows.
+        padding = 15
+
+        xlist = [x + x_offset for x in self.offsets]
+        ylist = [h - y_offset - i * padding for i in range(max_rows_per_page + 1)]
+
+        for rows in self.grouper(self.data, max_rows_per_page):
+            rows = tuple(filter(bool, rows))
+            self.writer.grid(xlist, ylist[:len(rows) + 1])
+            for y, row in zip(ylist[:-1], rows):
+                for x, cell in zip(xlist, row):
+                    self.writer.drawString(x + 2, y - padding + 3, str(cell))
+            self.writer.showPage()
+
+        self.writer.save()
+        return self.output.getvalue()
+
+
 __all__ = [
     'OutputTemplate',
     'BytesOutputWriter',
     'CSVBytesOutputWriter',
-    'XLSXBytesOutputWriter'
+    'XLSXBytesOutputWriter',
+    'PDFBytesOutputWriter'
 ]
